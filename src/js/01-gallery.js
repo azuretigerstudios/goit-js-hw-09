d3903b750fbf478bc74da1621df7366f4ea30272
azuretigerstudios <azuretigerstudios@gmail.com>
2025-10-27 01:40:08 +0300

    summary

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,21 +1,16 @@
-import SimpleLightbox from 'simple-lightbox';
+// import SimpleLightbox from './simple-lightbox.esm.js';
+
 import { galleryItems } from './gallery-items.js';
 
 const galleryContainer = document.querySelector('.gallery');
-const galleryMarkup = galleryItems
+galleryContainer.innerHTML = galleryItems
   .map(
     ({ preview, original, description }) => `
       <a class="gallery__item" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          alt="${description}"
-        />
+        <img class="gallery__image" src="${preview}" alt="${description}" />
       </a>`
   )
   .join('');
-
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
