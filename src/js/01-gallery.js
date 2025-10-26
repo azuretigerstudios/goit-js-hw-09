cfa044f2e0feaab66dabab0cbad7025484a9c5e0
azuretigerstudios <azuretigerstudios@gmail.com>
2025-10-27 00:41:51 +0300

    gallery update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,4 @@
-import SimpleLightbox from 'simple-lightbox.esm.js';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
